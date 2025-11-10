49605eb10aa84eaaf17b69b59f6fe9314a19d38b
deeprogra <61528474+deeprogra@users.noreply.github.com>
2025-11-09 18:32:05 -0600

    feat: get beans

--- src/main/java/com/deeprogra/store/OrderService.java
+++ src/main/java/com/deeprogra/store/OrderService.java
@@ -1,11 +1,16 @@
 package com.deeprogra.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-    // public OrderService(PaymentService paymentService) {
-    //     this.paymentService = paymentService;
-    // }
+    @Autowired
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
