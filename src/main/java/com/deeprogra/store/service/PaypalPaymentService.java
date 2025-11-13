d7b7628f35137519166967e35ecbf7b4dfd86226
deeprogra <61528474+deeprogra@users.noreply.github.com>
2025-11-13 09:10:29 -0600

    feat: add logs

--- src/main/java/com/deeprogra/store/service/PaypalPaymentService.java
+++ src/main/java/com/deeprogra/store/service/PaypalPaymentService.java
@@ -1,14 +1,18 @@
 package com.deeprogra.store.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaypalPaymentService implements PaymentService {
 
+    Logger logger = LoggerFactory.getLogger(PaypalPaymentService.class);
+
     @Override
     public void processPayment(double amount) {
-        System.out.println("PAYPAL");
-        System.out.println("Amount: " + amount);
+        logger.info("PAYPAL");
+        logger.debug("Amount: " + amount);
     }
 
 }
--- src/main/java/com/deeprogra/store/service/StripePaymentService.java
+++ src/main/java/com/deeprogra/store/service/StripePaymentService.java
@@ -1,11 +1,16 @@
 package com.deeprogra.store.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StripePaymentService implements PaymentService {
+
+    Logger logger = LoggerFactory.getLogger(StripePaymentService.class);
 
     @Override
     public void processPayment(double amount) {
-        System.out.println("STRIPE");
-        System.out.println("Amount: " + amount);
+        logger.info("STRIPE");
+        logger.debug("Amount: " + amount);
     }
 
 }
